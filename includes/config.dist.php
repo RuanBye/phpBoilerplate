92abf3ff6aae54b5dee9e878b5d891c25524e759
RuanBye <43596581+RuanBye@users.noreply.github.com>
2023-02-13 05:10:13 +0200

    Initial commit

---
+++ includes/config.dist.php
@@ -0,0 +1,7 @@
+<?php
+return array(
+  'db_host' => 'localhost', // Database host
+  'db_name' => 'php-boilerplate', // Database name
+  'db_user' => 'root', // Database user
+  'db_pass' => '' // Database password
+);
